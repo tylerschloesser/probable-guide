4423f01f557ccb02e872e3ac58d7ea81d1c2e400
Tyler (win-ubuntu-vbox) <tylerschloesser@gmail.com>
2022-04-10 21:50:12 -0700

    set webpack stats to minimal

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,8 +2,5 @@ import webpack from 'webpack'
 
 module.exports = <webpack.Configuration>{
   entry: './src/index.ts',
-  // output: {
-  //   path: __dirname + '/dist',
-  //   filename: 'bundle.js',
-  // },
+	stats: 'minimal',
 }
