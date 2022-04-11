ae4388440f46b2fc4528ebcae7cddb1c1f440879
Tyler (win-ubuntu-vbox) <tylerschloesser@gmail.com>
2022-04-10 22:10:40 -0700

    black canvas

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
 const canvas = document.querySelector('canvas')!
-console.log(canvas)
+const rect = canvas.getBoundingClientRect()
+canvas.width = rect.width
+canvas.height = rect.height
+
+const context = canvas.getContext('2d')!
+
+context.fillStyle = '#111'
+context.fillRect(0, 0, canvas.width, canvas.height)
