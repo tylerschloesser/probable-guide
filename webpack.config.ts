a1a0f84ae4649d859a7d43bed402f8b444475226
Tyler (win-ubuntu-vbox) <tylerschloesser@gmail.com>
2022-04-10 21:45:55 -0700

    add partial webpack.config.ts

---
+++ webpack.config.ts
@@ -0,0 +1,9 @@
+import webpack from 'webpack'
+
+module.exports = <webpack.Configuration>{
+  entry: './src/index.ts',
+  // output: {
+  //   path: __dirname + '/dist',
+  //   filename: 'bundle.js',
+  // },
+}
