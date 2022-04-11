934ef0909f05803ff0d6a75313d15d66553b8767
Tyler (win-ubuntu-vbox) <tylerschloesser@gmail.com>
2022-04-10 22:00:26 -0700

    add index.html

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,6 +1,12 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin'
 import webpack from 'webpack'
 
 module.exports = <webpack.Configuration>{
   entry: './src/index.ts',
   stats: 'minimal',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
 }
