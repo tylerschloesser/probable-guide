8cac1a2a743af7d3da512caa4a40551abe692c36
Tyler (win-ubuntu-vbox) <tylerschloesser@gmail.com>
2022-04-10 22:05:12 -0700

    use ts-loader

--- webpack.config.ts
+++ webpack.config.ts
@@ -4,6 +4,18 @@ import webpack from 'webpack'
 module.exports = <webpack.Configuration>{
   entry: './src/index.ts',
   stats: 'minimal',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
