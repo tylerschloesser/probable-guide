fccab125e5202cd7d9ccc5c79eae997ce95cbd2d
Tyler (win-ubuntu-vbox) <tylerschloesser@gmail.com>
2022-04-13 12:34:55 -0700

    add jest

---
+++ src/util.test.ts
@@ -0,0 +1,9 @@
+import { calc_motion_window } from './util'
+
+describe('util', () => {
+  describe('calc_motion_window', () => {
+    it('does nothing', () => {
+      calc_motion_window([], 0)
+    })
+  })
+})
