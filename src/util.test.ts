d0d1b6b596eb35ac31b35f6bd3cb8553da71dd2b
Tyler (win-ubuntu-vbox) <tylerschloesser@gmail.com>
2022-04-13 12:37:22 -0700

    add base test cases

--- src/util.test.ts
+++ src/util.test.ts
@@ -2,8 +2,14 @@ import { calc_motion_window } from './util'
 
 describe('util', () => {
   describe('calc_motion_window', () => {
-    it('does nothing', () => {
-      calc_motion_window([], 0)
+    test('empty motion', () => {
+      expect(calc_motion_window([], 0)).toEqual({ dx: 0, dy: 0 })
+    })
+    test('one motion', () => {
+      expect(calc_motion_window([{ x: 1, y: 1, timestamp: 1 }], 0)).toEqual({
+        dx: 0,
+        dy: 0,
+      })
     })
   })
 })
--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,10 @@
 import { MotionEntry } from './common'
 
-export function calc_motion_window(motion: MotionEntry[], window_ms: number) {}
+export function calc_motion_window(motion: MotionEntry[], window_ms: number) {
+  if (motion.length < 2) {
+    return {
+      dx: 0,
+      dy: 0,
+    }
+  }
+}
